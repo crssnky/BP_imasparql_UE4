90a0954f41d33c99162c30b73bebe473fbe292b6
crssnky <vindicare28@yahoo.co.jp>
2018-12-28 02:02:22 +0900

    make Plugin

---
+++ Plugins/Developer/imasparql/Source/imasparql/Public/imasparqlBP.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "imasparqlBP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IMASPARQL_API UimasparqlBP : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
